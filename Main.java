fd6f3a1671b5b3f6934e5075c526d6a8ec28c267
Swagatika <39233686+swagatikapanda123@users.noreply.github.com>
2019-02-08 19:57:43 +0530

    commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int t1=78, t2=89,t3=70;
+        double marks= 0;
+        double percentage;
+        marks = t1*0.30+t2*.4+t3*0.6;
+        percentage= marks/300*100;
+        System.out.println("marks="+ marks +" percentage is: "+percentage);
+    }
+}
